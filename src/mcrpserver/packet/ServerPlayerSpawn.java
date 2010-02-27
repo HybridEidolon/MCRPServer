db1f9f0b827cfbdf83a489bce6562f8145b70303
Furyhunter <furyhunter600@gmail.com>
2010-02-27 01:28:30 -0600

    Implemented ServerLevelFinalize, ServerPlayerSpawn, ServerSetBlock packets.

---
+++ src/mcrpserver/packet/ServerPlayerSpawn.java
@@ -0,0 +1,85 @@
+/*
+ *  Copyright (C) 2010 Furyhunter
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package mcrpserver.packet;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.charset.Charset;
+
+/**
+ *
+ * @author Furyhunter
+ */
+public class ServerPlayerSpawn extends Packet {
+
+    private byte playerid;
+    private String playername;
+    private short xpos;
+    private short ypos;
+    private short zpos;
+    private byte heading;
+    private byte pitch;
+
+    public ServerPlayerSpawn(byte playerid, String playername, short x,
+            short y, short z, byte heading, byte pitch) {
+        this.id = OpCode.SERVER_PLAYER_SPAWN;
+        this.playerid = playerid;
+        this.playername = playername;
+        this.xpos = x;
+        this.ypos = y;
+        this.zpos = z;
+        this.heading = heading;
+        this.pitch = pitch;
+    }
+
+    @Override
+    public byte[] build() {
+        ByteBuffer pkt = ByteBuffer.allocate(1024);
+        pkt.order(ByteOrder.BIG_ENDIAN);
+
+        // put id
+        pkt.put((byte)id.id);
+
+        // put player id
+        pkt.put(playerid);
+
+        // put player name
+        byte[] msg;
+        try {
+            msg = playername.substring(0,63)
+                    .getBytes(Charset.forName("US-ASCII"));
+        } catch (StringIndexOutOfBoundsException ex) {
+            msg = playername.getBytes(Charset.forName("US-ASCII"));
+        }
+        byte[] fill = new byte[64-msg.length];
+        pkt.put(msg);
+        pkt.put(fill);
+
+        // put x y z
+        pkt.putShort(xpos);
+        pkt.putShort(ypos);
+        pkt.putShort(zpos);
+
+        // put heading, pitch
+        pkt.put(heading);
+        pkt.put(pitch);
+
+        pkt.put((byte)0x0A);
+
+        return pkt.array();
+    }
+}
---
+++ src/mcrpserver/packet/ServerSetBlock.java
@@ -0,0 +1,65 @@
+/*
+ *  Copyright (C) 2010 Furyhunter
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package mcrpserver.packet;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+/**
+ *
+ * @author Furyhunter
+ */
+public class ServerSetBlock extends Packet {
+
+    private short xpos;
+    private short ypos;
+    private short zpos;
+    private byte blocktype;
+
+    public ServerSetBlock(short x, short y, short z, byte type) {
+        this.id = OpCode.SERVER_SET_BLOCK;
+        this.xpos = x;
+        this.ypos = y;
+        this.zpos = z;
+        this.blocktype = type;
+    }
+
+    @Override
+    public byte[] build() {
+        ByteBuffer pkt = ByteBuffer.allocate(1024);
+        pkt.order(ByteOrder.BIG_ENDIAN);
+
+        // put id
+        pkt.put((byte)id.id);
+
+        // put x
+        pkt.putShort(xpos);
+
+        // put y
+        pkt.putShort(ypos);
+
+        // put z
+        pkt.putShort(zpos);
+
+        // put blocktype
+        pkt.put(blocktype);
+
+        pkt.put((byte)0x0A);
+
+        return pkt.array();
+    }
+}
