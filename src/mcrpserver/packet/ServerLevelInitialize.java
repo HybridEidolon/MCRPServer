5d53614ff2140a95548f8a810ad0dd9a6943e28f
Furyhunter <furyhunter600@gmail.com>
2010-02-27 00:59:29 -0600

    Implemented ServerLevelDataChunk, ServerLevelInitialize, ServerPing packets.

---
+++ src/mcrpserver/packet/ServerLevelInitialize.java
@@ -0,0 +1,44 @@
+/*
+ *  Copyright (C) 2010 Furyhunter
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package mcrpserver.packet;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+/**
+ *
+ * @author Furyhunter
+ */
+public class ServerLevelInitialize extends Packet {
+
+    public ServerLevelInitialize() {
+        this.id = OpCode.SERVER_LEVEL_INIT;
+    }
+
+    @Override
+    public byte[] build() {
+        ByteBuffer pkt = ByteBuffer.allocate(2);
+        pkt.order(ByteOrder.BIG_ENDIAN);
+
+        // put id
+        pkt.put((byte)id.id);
+
+        pkt.put((byte)0x0A);
+        
+        return pkt.array();
+    }
+}
